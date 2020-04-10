1ca313996ff5c4446443d80d92581929b278a67a
Elaaf Salem <32464223+ElaafSalem@users.noreply.github.com>
2020-04-11 01:13:20 +0300

    Updated and organized

--- UserParser/UserParserComponent.py
+++ UserParser/UserParserComponent.py
@@ -3,33 +3,35 @@
 #machine learning compnent and could be used to display to the 
 #user the parsed output of flowdroid.
 #################################################################
+  
 
-#import pandas as pd
 import os
 import subprocess
 import re
 import timeit
 from os import path
+
 #INPUT
-#pathFD = "*_FD.txt"
-ParseFDclassPath = "*.class folder"
+pathFD = "/Users/elaafsalem/Downloads/AMD_Anlys/Test/357d366f9b43e8f1acb334d57e7559b3_FD.txt"
+classPath = "/Users/elaafsalem/Graduation Project/"
 
-
-############################ParseFD##############################
-#############################INPUT###############################
-#classPath: put the path of the ParseFD.class folder
-#FDtxtPath: put the path of the output of flowdroid *_FD.txt
-############################OUTPUT###############################
-#The path of the new parsed file *_P.txt
-##########################DESCRIPTION############################
-#This function takes the output of flowdroid and parse it in the
-#form of <src> ~> <snk>, It takes only one file The output could 
-#be used to be displayed to the user directly through one of the
-#commandline options or could be used to get to the final stage 
-#of the parser (FD>P) in order to be used in the machine learning
-#model to predict if the app have a malicious behavior.
-#################################################################
-
+MLcolumnsList = [
+       '<java.net.URL: java.net.URLConnection openConnection()> ~> <java.net.HttpURLConnection: void setRequestMethod(java.lang.String)>',
+       '<android.os.Bundle: java.lang.String getString(java.lang.String)> ~> <android.widget.TextView: void setText(java.lang.CharSequence)>',
+       '<android.telephony.TelephonyManager: java.lang.String getDeviceId()> ~> <org.apache.http.message.BasicNameValuePair: void init(java.lang.String,java.lang.String)>',
+       '<android.app.PendingIntent: android.app.PendingIntent getBroadcast(android.content.Context,int,android.content.Intent,int)> ~> <android.telephony.SmsManager: void sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)>',
+       '<android.telephony.TelephonyManager: java.lang.String getSubscriberId()> ~> <android.os.Bundle: void putString(java.lang.String,java.lang.String)>',
+       '<android.telephony.TelephonyManager: java.lang.String getLine1Number()> ~> <android.util.Log: int v(java.lang.String,java.lang.String)>',
+       '<android.app.PendingIntent: android.app.PendingIntent getActivity(android.content.Context,int,android.content.Intent,int)> ~> <android.telephony.SmsManager: void sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)>',
+       '<android.app.Activity: android.content.Intent getIntent()> ~> <android.util.Log: int w(java.lang.String,java.lang.String)>',
+       '<android.app.PendingIntent: android.app.PendingIntent getBroadcast(android.content.Context,int,android.content.Intent,int)> ~> <android.content.Context: android.content.ComponentName startService(android.content.Intent)>',
+       '<com.google.ads.AdActivity: android.content.Intent getIntent()> ~> <android.util.Log: int e(java.lang.String,java.lang.String)>',
+       '<android.os.Bundle: java.lang.String getString(java.lang.String)> ~> <android.os.Bundle: void putString(java.lang.String,java.lang.String)> ',
+       '<android.os.Bundle: java.lang.String getString(java.lang.String)> ~> <android.util.Log: int d(java.lang.String,java.lang.String)> ',
+       '<android.os.Bundle: java.lang.String getString(java.lang.String)> ~> <android.util.Log: int e(java.lang.String,java.lang.String)> ',
+       '<android.os.Bundle: int getInt(java.lang.String)> ~> <android.os.Bundle: void putInt(java.lang.String,int)> ',
+       '<android.os.Bundle: java.io.Serializable getSerializable(java.lang.String)> ~> <android.os.Bundle: void putSerializable(java.lang.String,java.io.Serializable)> ']
+  
 
 
 def finderror(path):
@@ -40,6 +42,7 @@ def finderror(path):
             exit(0)
 
 
+#######CHANGED TO BE DELETED##########################################
 def ParseFD(classPath, FDtxtPath ,Analysis_Output_dir):
     os.chdir(classPath)
     resultP= Analysis_Output_dir+"/"+ path.basename(FDtxtPath).replace('_FD','_P')
@@ -51,80 +54,57 @@ def ParseFD(classPath, FDtxtPath ,Analysis_Output_dir):
     print(FDtxtPath+' '+Analysis_Output_dir)
     return (resultP)
   #  return (FDtxtPath[:-6]+"P.txt")
+######################################################################
 
 
 
-############################ParseP###############################
+
+###########################fillTemplate###############################
 #############################INPUT###############################
-#path: put the path of the *_P.txt file
+#MLcolumnsList: selected features (columns) from the dataset
+#pathFD: flowdroid txt file path
 ############################OUTPUT###############################
-#List of ["AppName", "Source","Sink"]
+#List of ["AppName", zeros and ones]
+#example ["AppX", 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ##########################DESCRIPTION############################
-#This function takes the parsed output of flowdroid and parse it
-#in the form of a list as decribed above. It's the final stage 
-#of the parser (FD>P) and the output is used in the machine 
-#learning model to predict if the app have a malicious behavior.
-#You can have the *_P.txt from the output of the 
-#ParseFD(classPath, FDtxtPath) function
-#################################################################
-def parseP(path):
+#This function takes the output of flowdroid and parse it
+#in the form of "src ~> snk". Then it fit its dataflows to our
+#dataset (exclude missing features). The output of this function 
+#is used in the machine learning model to predict if the app have 
+#a malicious behavior.
+######################################################################
+def fillTemplate(MLcolumnsList,pathFD):
+    os.chdir(classPath)
+    process = subprocess.Popen('java ParseFD '+pathFD, shell=True, stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+    process=process.communicate()
+    print("-------------------------------------------")
 
-    #get the file name without the extension
-    name = os.path.basename(path)[:-6]
-    
-    #read and add the dataflows of the app
-    dataflows = []
-    with open(path) as f:
-        for line in f:
-            dataflows.append(line)
-            #print(line)
-    
-    #prepare the app data to send it to the ML model
-    names = []
-    src=[]
-    snk=[]
-    if(dataflows==[]):
-        names.append(name)
-        src.append("NO_SENSITIVE_SOURCE")
-        snk.append("NO_SENSITIVE_SINK")
-    else:
-        for data in dataflows:
-            names.append(name)
-            index = re.match(r".*\s~>", data).span()[1]-1 #get the index of the arrow
-            src.append(data[:(index-1)]) #before the arrow: src
-            snk.append(data[(index+2):-1]) #after the arrow: snk
-    App = [names, src, snk] #zip them together
-    return App
+    parsedFilePath = (pathFD[:-6]+"P.txt")
 
-###########################EXAMPLE###############################
-#parseP(ParseFD(ParseFDclassPath, pathFD))
-
-
-
-
-#########################fillTemplate############################
-# FOR MACHINE LEARNING MODEL, needs more testing
-#################################################################
-
-def fillTemplate(MLcolumnsList, parsedFilePath):
     dataflows = []
     tempList = []
+    
     with open(parsedFilePath) as f:
         for line in f:
-            dataflows.append(line)
-            print(line)
-            
+            dataflows.append(line[:-1])
+
     tempList.append(os.path.basename(parsedFilePath)[:-6])
 
-    if(dataflows == [] & ("NO_SENSITIVE_SOURCE ~> NO_SENSITIVE_SINK" in MLcolumnsList)):
+
+    
+    if((dataflows == []) & ("NO_SENSITIVE_SOURCE ~> NO_SENSITIVE_SINK" in MLcolumnsList)):
         dataflows.append("NO_SENSITIVE_SOURCE ~> NO_SENSITIVE_SINK")
-        
-    else:
-        return [0]*len(MLcolumnsList)
-        
-    for i in tempList:      
-        if i in MLcolumnsList:
-                tempList.append(1)
+    
+    for i in MLcolumnsList:    
+        if i in dataflows:
+            tempList.append(1)
         else:
-                tempList.append(0)
+            tempList.append(0)
+            
     return tempList
+
+
+###########################EXAMPLE###############################
+print(fillTemplate(MLcolumnsList,pathFD))
+
