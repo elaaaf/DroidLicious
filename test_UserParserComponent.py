3615402090dc3332d9054fa0056a5f6bd8b1b7c3
SHRQ1 <sheeroolt@gmail.com>
2020-04-19 10:59:03 +0300

    Testing Commit

--- test_UserParserComponent.py
+++ test_UserParserComponent.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 import UserParser.UserParserComponent
-
+import os
 Error_msgFD='C:\\Users\shosho\DroidLicious\Analysis_Output\\4G_Speed_UC_FD.txt' # contain error masg (
 file='F:\Project\AndroMalyZer_Dataset\APK_NotYetIncluded\\apk_storage1_114apk\com-kiddoware-kidspictureviewer\com_kiddoware_kidspictureviewer-8_FD.txt'
+output_folder = os.getcwd() + "/Analysis_Output/"
 
 class TestCase1(TestCase):
 
@@ -10,10 +11,11 @@ class TestCase1(TestCase):
         with self.assertRaises(SystemExit):
             UserParser.UserParserComponent.finderror(Error_msgFD)
 
-    def test_fill_template(self): # ensure the output of the temp list equle to the number of the model's features
-        self.assertEqual(UserParser.UserParserComponent.fillTemplate(file,).__len__(),16)
+
 
 class TestCase2(TestCase):
-
- def test_finderror(self): #no error find
+    def test_finderror(self): #no error find
         self.assertEqual(UserParser.UserParserComponent.finderror(file),None)
+
+    def test_fill_template(self): # ensure the output of the temp list equle to the number of the model's features
+        self.assertEqual(UserParser.UserParserComponent.fillTemplate(file,output_folder).__len__(),16)
